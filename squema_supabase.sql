97545d773df5c4e286dba7f431db928612896f0f
NashRusten <jpardo180@gmail.com>
2025-09-17 14:04:08 -0500

    tipo MIME mas tolerante

--- squema_supabase.sql
+++ squema_supabase.sql
@@ -8,6 +8,14 @@ CREATE TABLE public.alimentacion (
   updated_at timestamp without time zone,
   CONSTRAINT alimentacion_pkey PRIMARY KEY (ali_id)
 );
+CREATE TABLE public.autorizados (
+  id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
+  num_cedula character varying NOT NULL UNIQUE,
+  nombre character varying NOT NULL,
+  created_at timestamp without time zone DEFAULT now(),
+  updated_at timestamp without time zone,
+  CONSTRAINT autorizados_pkey PRIMARY KEY (id)
+);
 CREATE TABLE public.bancos (
   ban_id integer NOT NULL DEFAULT nextval('bancos_ban_id_seq'::regclass),
   ban_banco_nombre character varying NOT NULL UNIQUE,
@@ -28,10 +36,8 @@ CREATE TABLE public.datos_bancarios (
   created_at timestamp without time zone DEFAULT now(),
   updated_at timestamp without time zone,
   dba_banco_id integer NOT NULL,
-  dba_tcb_id integer NOT NULL,
   CONSTRAINT datos_bancarios_pkey PRIMARY KEY (dba_id),
-  CONSTRAINT datos_bancarios_dba_banco_id_fkey FOREIGN KEY (dba_banco_id) REFERENCES public.bancos(ban_id),
-  CONSTRAINT datos_bancarios_dba_tcb_id_fkey FOREIGN KEY (dba_tcb_id) REFERENCES public.tipo_cuenta_bancaria(tcb_id)
+  CONSTRAINT datos_bancarios_dba_banco_id_fkey FOREIGN KEY (dba_banco_id) REFERENCES public.bancos(ban_id)
 );
 CREATE TABLE public.epss (
   eps_id integer NOT NULL DEFAULT nextval('epss_eps_id_seq'::regclass),
@@ -66,7 +72,7 @@ CREATE TABLE public.personal (
   per_segundo_nombre character varying,
   per_primer_apellido character varying NOT NULL,
   per_segundo_apellido character varying,
-  per_telefono_llamada character varying NOT NULL,
+  per_telefono_llamada character varying,
   per_telefono_whatsapp character varying NOT NULL,
   per_fecha_nacimiento date NOT NULL,
   per_correo character varying NOT NULL,
@@ -75,8 +81,8 @@ CREATE TABLE public.personal (
   per_telefono_contacto_emergencia character varying NOT NULL,
   per_estatura smallint NOT NULL,
   per_talla_camiseta character varying NOT NULL,
-  per_talla_pantalon character varying NOT NULL,
-  per_talla_zapatos character varying NOT NULL,
+  per_talla_pantalon character varying,
+  per_talla_zapatos character varying,
   per_localidad_barrio character varying NOT NULL,
   per_alergias character varying,
   per_nivel_ingles character varying NOT NULL,
@@ -84,7 +90,7 @@ CREATE TABLE public.personal (
   updated_at timestamp without time zone,
   per_genero_id integer NOT NULL,
   per_ciudad_id integer NOT NULL,
-  per_eps_id integer NOT NULL,
+  per_eps_id integer,
   per_ali_id integer NOT NULL,
   per_dato_bancario integer NOT NULL,
   per_status_personal integer NOT NULL,
@@ -92,12 +98,12 @@ CREATE TABLE public.personal (
   per_foto text,
   CONSTRAINT personal_pkey PRIMARY KEY (per_id),
   CONSTRAINT personal_per_genero_id_fkey FOREIGN KEY (per_genero_id) REFERENCES public.generos(gen_id),
-  CONSTRAINT personal_per_perfilamiento_fkey FOREIGN KEY (per_perfilamiento) REFERENCES public.perfilamiento(perf_id),
-  CONSTRAINT personal_per_status_personal_fkey FOREIGN KEY (per_status_personal) REFERENCES public.status_personal(spe_id),
-  CONSTRAINT personal_per_dato_bancario_fkey FOREIGN KEY (per_dato_bancario) REFERENCES public.datos_bancarios(dba_id),
-  CONSTRAINT personal_per_ali_id_fkey FOREIGN KEY (per_ali_id) REFERENCES public.alimentacion(ali_id),
+  CONSTRAINT personal_per_ciudad_id_fkey FOREIGN KEY (per_ciudad_id) REFERENCES public.ciudades(ciu_id),
   CONSTRAINT personal_per_eps_id_fkey FOREIGN KEY (per_eps_id) REFERENCES public.epss(eps_id),
-  CONSTRAINT personal_per_ciudad_id_fkey FOREIGN KEY (per_ciudad_id) REFERENCES public.ciudades(ciu_id)
+  CONSTRAINT personal_per_ali_id_fkey FOREIGN KEY (per_ali_id) REFERENCES public.alimentacion(ali_id),
+  CONSTRAINT personal_per_dato_bancario_fkey FOREIGN KEY (per_dato_bancario) REFERENCES public.datos_bancarios(dba_id),
+  CONSTRAINT personal_per_status_personal_fkey FOREIGN KEY (per_status_personal) REFERENCES public.status_personal(spe_id),
+  CONSTRAINT personal_per_perfilamiento_fkey FOREIGN KEY (per_perfilamiento) REFERENCES public.perfilamiento(perf_id)
 );
 CREATE TABLE public.status_personal (
   spe_id integer NOT NULL DEFAULT nextval('status_personal_spe_id_seq'::regclass),
@@ -106,11 +112,4 @@ CREATE TABLE public.status_personal (
   created_at timestamp without time zone DEFAULT now(),
   updated_at timestamp without time zone,
   CONSTRAINT status_personal_pkey PRIMARY KEY (spe_id)
-);
-CREATE TABLE public.tipo_cuenta_bancaria (
-  tcb_id integer NOT NULL DEFAULT nextval('tipo_cuenta_bancaria_tcb_id_seq'::regclass),
-  tcb_tipo_cuenta character varying NOT NULL UNIQUE,
-  created_at timestamp without time zone DEFAULT now(),
-  updated_at timestamp without time zone,
-  CONSTRAINT tipo_cuenta_bancaria_pkey PRIMARY KEY (tcb_id)
 );
